8bccfed3724e489c9e51fef9b50252b96b6764b8
unknown <kostmetallist@gmail.com>
2023-06-14 20:59:07 +0300

    Provide fake trades endpoint for QSP demo

--- main.py
+++ main.py
@@ -17,6 +17,17 @@ def get_user(user_id: int):
     return [user for user in fake_users if user['id'] == user_id]
 
 
+fake_trades = [
+    {'id': 1, 'user_id': 3, 'currency_code': 'BTC', 'operation': 'buy', 'price': 123.45, 'amount': 42},
+    {'id': 2, 'user_id': 3, 'currency_code': 'BTC', 'operation': 'sell', 'price': 125.88, 'amount': 40},
+]
+
+
+@app.get('/trades')
+def get_trades(limit: int, offset: int = 0):
+    return fake_trades[offset:][:limit]
+
+
 @app.get('/')
 def hello():
     return 'hello world'
