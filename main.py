e2edfc572b75fcc9368dabd9e948f7244281a25b
unknown <kostmetallist@gmail.com>
2023-09-05 03:08:52 +0300

    Enable pydantic validation

--- main.py
+++ main.py
@@ -1,18 +1,61 @@
+from enum import Enum
+from datetime import datetime
+from typing import List, Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 
 app = FastAPI(
     title='Assets Compass'
 )
 
+
+class Rank(Enum):
+    rookie = 'rookie'
+    expert = 'expert'
+
+
+class Degree(BaseModel):
+    rank: Rank
+    date_approval: datetime
+
+
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+    degree: Optional[Degree]
+
+
 fake_users = [
-    {'id': 1, 'role': 'admin', 'name': 'Roger'},
-    {'id': 2, 'role': 'moderator', 'name': 'Mike'},
-    {'id': 3, 'role': 'associate', 'name': 'Ferdinand'},
+    {
+        'id': 1,
+        'role': 'admin',
+        'name': 'Roger',
+        'degree': {
+            'rank': Rank.expert,
+            'date_approval': datetime(year=2020, month=2, day=11),
+        },
+    },
+    {
+        'id': 2,
+        'role': 'moderator',
+        'name': 'Mike',
+    },
+    {
+        'id': 3,
+        'role': 'associate',
+        'name': 'Ferdinand',
+        'degree': {
+            'rank': Rank.rookie,
+            'date_approval': datetime.utcnow(),
+        },
+    },
 ]
 
 
-@app.get('/users/{user_id}')
+@app.get('/users/{user_id}', response_model=List[User])
 def get_user(user_id: int):
     return [user for user in fake_users if user['id'] == user_id]
 
@@ -24,6 +67,15 @@ def change_user_name(user_id: int, new_name: str):
     return {'status': 'OK'}
 
 
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency_code: str = Field(max_length=5)
+    operation: str
+    price: float = Field(ge=0)
+    amount: int
+
+
 fake_trades = [
     {'id': 1, 'user_id': 3, 'currency_code': 'BTC', 'operation': 'buy', 'price': 123.45, 'amount': 42},
     {'id': 2, 'user_id': 3, 'currency_code': 'BTC', 'operation': 'sell', 'price': 125.88, 'amount': 40},
@@ -33,6 +85,15 @@ fake_trades = [
 @app.get('/trades')
 def get_trades(limit: int, offset: int = 0):
     return fake_trades[offset:][:limit]
+
+
+@app.post('/trades')
+def add_trades(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return {
+        'status': 200,
+        'data': trades
+    }
 
 
 @app.get('/')
