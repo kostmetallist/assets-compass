309092869dd99fa15fe96f2a9c2136147f49f47e
unknown <kostmetallist@gmail.com>
2024-08-03 23:58:17 +0300

    Describe model

---
+++ models/models.py
@@ -0,0 +1,61 @@
+from datetime import datetime, UTC
+from functools import partial
+
+from sqlalchemy import MetaData, Table, Column, ForeignKey, Integer, String, JSON, TIMESTAMP
+
+
+metadata = MetaData()
+
+role = Table(
+    'role',
+    metadata,
+    Column(
+        'id',
+        Integer,
+        primary_key=True
+    ),
+    Column(
+        'name',
+        String,
+        nullable=False
+    ),
+    Column(
+        'permissions',
+        JSON
+    )
+)
+
+user = Table(
+    'user',
+    metadata,
+    Column(
+        'id',
+        Integer,
+        primary_key=True
+    ),
+    Column(
+        'email',
+        String,
+        nullable=False
+    ),
+    Column(
+        'username',
+        String,
+        nullable=False
+    ),
+    Column(
+        'password',
+        String,
+        nullable=False
+    ),
+    Column(
+        'registered_at',
+        TIMESTAMP,
+        default=partial(datetime.now, UTC)
+    ),
+    Column(
+        'role_id',
+        Integer,
+        ForeignKey(role.id)
+    )
+)
